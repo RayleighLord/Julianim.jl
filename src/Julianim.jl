df36bddad8a666de2821f34545d9650bc29e8b6e
Javier Gonzalez <javier.gonzalez.monge@gmail.com>
2023-01-15 10:53:22 +0100

    Added constants file

--- src/Julianim.jl
+++ src/Julianim.jl
@@ -4,30 +4,8 @@ using Reexport
 
 @reexport using GLMakie, LaTeXStrings
 
+include("contants.jl")
 include("themes/plotting_themes.jl")
-
-const COLORS = [
-    "#4263eb",
-    "#f03e3e",
-    "#f76707",
-    "#22b371",
-    "#5f3dc4",
-    "#fcc419",
-    "#c2255c",
-    "#1098ad",
-    "#cf6db0",
-]
-
-const MARKERS = [
-    :circle,
-    :utriangle,
-    :rect,
-    :dtriangle,
-    :diamond,
-    :xcross,
-    :hexagon,
-    :star5,
-]
 
 export COLORS, MARKERS
 
---
+++ src/contants.jl
@@ -0,0 +1,22 @@
+const COLORS = [
+    "#4263eb",
+    "#f03e3e",
+    "#f76707",
+    "#22b371",
+    "#5f3dc4",
+    "#fcc419",
+    "#c2255c",
+    "#1098ad",
+    "#cf6db0",
+]
+
+const MARKERS = [
+    :circle,
+    :utriangle,
+    :rect,
+    :dtriangle,
+    :diamond,
+    :xcross,
+    :hexagon,
+    :star5,
+]
